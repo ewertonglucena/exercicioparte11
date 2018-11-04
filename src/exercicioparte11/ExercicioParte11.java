317f9690c23136836fde33168bc2c238439a913c
ewertonglucena <44753431+ewertonglucena@users.noreply.github.com>
2018-11-04 21:37:37 -0200

    Adicionando Pendrive e Superclasse do exercicio passado

--- src/exercicioparte11/ExercicioParte11.java
+++ src/exercicioparte11/ExercicioParte11.java
@@ -1,10 +1,23 @@
-
 package exercicioparte11;
-
+import dados.*;
 public class ExercicioParte11 {
 
     public static void main(String[] args) {
-
+        Pendrive pen[] = new Pendrive[20];
+        
+        for(int i=0; i <pen.length; i++){
+            pen[i] = new Pendrive ();
+            pen[i].entradaDados();
+        }
+        String cab1 = "\033[31;1mMarca";
+        String cab2 = "\033[31;1mModelo";
+        String cab3 = "\033[31;1mCapacidade";
+        String cab4 = "\033[31;1mPreço";
+        
+        
+        System.out.println(cab1);
+        
+        
     }
     
 }
