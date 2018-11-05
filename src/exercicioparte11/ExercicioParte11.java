546eb7a023ccda36acd5e80c0a639138cab1c2c0
20141104662 <20141104662@TJINLA09E17.LABUVA.EDU>
2018-11-05 19:05:19 -0200

    aula

--- src/exercicioparte11/ExercicioParte11.java
+++ src/exercicioparte11/ExercicioParte11.java
@@ -1,13 +1,11 @@
 package exercicioparte11;
-import java.math.BigDecimal;
-import java.text.NumberFormat;
-import java.util.Locale;
+import java.text.DecimalFormat;
 import pendrive.*;
 public class ExercicioParte11 {
 
     public static void main(String[] args) {
    
-        Pendrive pen[] = new Pendrive[0];
+        Pendrive pen[] = new Pendrive[1];
     
         for(int i=0; i <pen.length; i++){
             pen[i] = new Pendrive ();
@@ -21,27 +19,25 @@ public class ExercicioParte11 {
         cabecalho[2] = "Capacidade";
         cabecalho[3] = "Preço";
         
-        String espaco = "   ";
+        String espaco = "\t";
         
         String t = "total";
         String q = "Quantidade";
         
-        System.out.println("\033[31;1m"+cabecalho[0]+espaco+cabecalho[1]+espaco+cabecalho[2]+espaco+cabecalho[3]);
+        System.out.println("\033[31;1m"+cabecalho[0]+"\t"+cabecalho[1]+"\t"+cabecalho[2]+"\t"+cabecalho[3]);
         System.out.print("\033[31;1m");
         for(int i = 0; i<cabecalho.length; i++){
             for(int j = 0; j<cabecalho[i].length(); j++){
                 System.out.print("-");
             }
-            System.out.print(espaco);
+            System.out.print("\t");
         }
-
-        NumberFormat numberFormat = NumberFormat.getInstance();
-
+        
+        DecimalFormat p = new DecimalFormat("#,##0.00");
         
         for(int i = 0; i<pen.length;i++){
-            String number = numberFormat.format(pen[i].getPreco());
-            //BigDecimal bigDecimal = BigDecimal.valueOf(number.doubleValue());
-            System.out.print("\n\033[32;1m"+pen[i].getMarca()+espaco+pen[i].getModelo()+espaco+pen[i].getCapacidade()+espaco+number);
+
+            System.out.print("\n\033[32;1m"+pen[i].getMarca()+espaco+pen[i].getModelo()+espaco+pen[i].getCapacidade()+espaco+p.format(pen[i].getPreco()));
         }
         
         System.out.print("\n\033[34;1m");
@@ -51,8 +47,22 @@ public class ExercicioParte11 {
             }
             System.out.print(espaco);
         }
-        System.out.println("\n\033[34;1m"+cabecalho[2]+" total: ");
         
+        int somaCapacidade = 0;
+        
+        for(int i=0; i<pen.length; i++){
+            somaCapacidade+=pen[i].getCapacidade();
+        }
+        System.out.print("\n\033[34m"+cabecalho[2]+" total: "+somaCapacidade);
+        
+        double precoTotal = 0;
+        
+        for(int i=0; i<pen.length; i++){
+            precoTotal+=pen[i].getPreco();
+        }
+        System.out.print("\n\033[34m"+cabecalho[3]+" total: "+precoTotal);
+        
+        System.out.print("\n\033[34mQuantidade: "+pen.length);
     }
     
 }
